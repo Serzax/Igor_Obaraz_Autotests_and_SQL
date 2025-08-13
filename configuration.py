0bdced34e343e639d31da11127883a2df4961986
Serzax <skillz96@yandex.ru>
2025-08-13 20:32:13 +0100

    Автотест Яндекс Самокат

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://d089465a-bfbf-4644-a9c8-4a471b23cd91.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://e2d0f555-a199-4bd2-a9f3-06ce74a0a268.serverhub.praktikum-services.ru"
 DOC_PATH = "/docs/"
 CREATE_ORDER_PATH = "/api/v1/orders"
 TRACK_ORDER_PATH = "/api/v1/orders/track?t="
